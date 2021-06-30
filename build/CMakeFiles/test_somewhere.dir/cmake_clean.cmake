8d56bce7d016a61101166ab54ca122ff6348b0bc
nekida <nekida@gmail.com>
2021-06-30 11:57:11 +0300

    first commit

---
+++ build/CMakeFiles/test_somewhere.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_somewhere.dir/test_somewhere.c.o"
+  "test_somewhere"
+  "test_somewhere.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_somewhere.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test_somewhere.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define SIZE 5
+
+int src_array[SIZE] = {0};
+int dst_array[SIZE] = {0};
+
+int main (int argc, char *argv[])
+{
+    for (int i = 0; i < SIZE; i++)
+        src_array[i] = i;
+
+    for (int i = 0; i < SIZE; i++)
+        printf("%d ", src_array[i]);
+
+    printf("\n");
+
+    int j;
+    for (int i = 0, j = 0; i < SIZE; ) {
+        int needed_num = *argv[i + 1] - '0';
+        if (needed_num == src_array[j]) {
+            dst_array[i++] = src_array[j++];
+        } else {
+            j++;
+        }
+        if (j == SIZE) j = 0;
+    }
+
+    for (int i = 0; i < SIZE; i++)
+        printf("%d ", dst_array[i]);
+
+    printf("\n");
+
+    return 0;
+}
