24a3c0b5d58c677e4bf2a9570b613e96cbbf7c57
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2021-07-01 10:33:33 +0300

    Update test_somewhere.c

--- test_somewhere.c
+++ test_somewhere.c
@@ -8,23 +8,23 @@ int dst_array[SIZE] = {0};
 
 int main (int argc, char *argv[])
 {
-    for (int i = 0; i < SIZE; i++)
+    int i, j;
+    for (i = 0; i < SIZE; i++)
         src_array[i] = i;
 
-    for (int i = 0; i < SIZE; i++)
+    for (i = 0; i < SIZE; i++)
         printf("%d ", src_array[i]);
 
     printf("\n");
 
-    int j;
-    for (int i = 0, j = 0; i < SIZE; ) {
+    for (i = 0, j = 0; i < SIZE; ) {
         int needed_num = *argv[i + 1] - '0';
         (needed_num == src_array[j]) ? (dst_array[i++] = src_array[j++]) : (j++);
         if (j == SIZE) 
             j = 0;
     }
 
-    for (int i = 0; i < SIZE; i++)
+    for (i = 0; i < SIZE; i++)
         printf("%d ", dst_array[i]);
 
     printf("\n");
